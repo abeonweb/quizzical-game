5bc2f16ee3b17de2e9e2782b2fa2296da6da6a1c
abeonweb <bmospecific@yahoo.com>
2023-02-02 22:33:12 +0100

    registration form added

---
+++ src/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// create a user and redirect to game
+//access to login page
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
