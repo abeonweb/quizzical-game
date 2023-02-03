58b1a12e8090601c91ad8aec5c1ebbee25ab1361
abeonweb <bmospecific@yahoo.com>
2023-02-03 08:36:51 +0100

    added state and context for game play

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import StartPage from "./StartPage";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
@@ -5,19 +6,24 @@ import Login from "./Login";
 import ScorePage from "./ScorePage";
 import Register from "./Register";
 import Game from "./Game";
+import { GameContext } from "./GameContext";
 
 export default function App() {
+  const [mode, setMode] = useState("easy");
+
   return (
-    <main className="main">
-      <Routes>
-        <Route path="/quizzical-game" element={<Layout />}>
-          <Route index element={<StartPage />} />
-          <Route path="game" element={<Game />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="score-page" element={<ScorePage />} />
-        </Route>
-      </Routes>
-    </main>
+    <GameContext.Provider value={mode}>
+      <main className="main">
+        <Routes>
+          <Route path="/quizzical-game" element={<Layout />}>
+            <Route index element={<StartPage setMode={setMode} />} />
+            <Route path="game" element={<Game />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="score-page" element={<ScorePage />} />
+          </Route>
+        </Routes>
+      </main>
+    </GameContext.Provider>
   );
 }
