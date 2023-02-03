24d044a8dcf344075b34e754b46fd9c546d510d8
abeonweb <bmospecific@yahoo.com>
2023-02-03 23:28:52 +0100

    disable button until mode is selected

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default function StartPage(props) {
-  const {setMode}= props
+  const {state, dispatch}= props
   
   return (
     
@@ -12,21 +12,24 @@ export default function StartPage(props) {
         </p>
         <div className="game-mode">
           <p>Select a play mode</p>
-          <button className="btn-mode" onClick={() => setMode("easy")}>
+          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "easy"})}>
             Easy
           </button>
-          <button className="btn-mode" onClick={() => setMode("medium")}>
+          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "medium"})}>
             Medium
           </button>
-          <button className="btn-mode" onClick={() => setMode("hard")}>
+          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "hard"})}>
             Hard
           </button>
         </div>
-        {
-          <Link className="btn btn--start" to="game">
+        
+          <Link 
+          className={`btn ${!state.start? "btn--disabled": null}`} 
+          to="game"
+          >
             Start quiz
           </Link>
-        }
+        
       </div>
   );
 }
