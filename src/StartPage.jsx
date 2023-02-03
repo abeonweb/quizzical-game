41a913236f997479248e85440a1ed2bcd7b34223
abeonweb <bmospecific@yahoo.com>
2023-02-03 08:37:36 +0100

    added functionality to select game mode

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function StartPage() {
-  const [mode, setMode] = useState("easy");
-
+export default function StartPage(props) {
+  const {setMode}= props
+  
   return (
-    <>
+    
       <div className="start-page">
         <h1 className="start-page--title">quizzical</h1>
         <p className="start-page--tagline">
           A fun trivia game you can enjoy with friends!
         </p>
-        <button className="btn-mode" onClick={()=> setMode("easy")}>Easy</button>
-        <button className="btn-mode" onClick={()=> setMode("medium")}>Medium</button>
-        <button className="btn-mode" onClick={()=> setMode("hard")}>Hard</button>
-        {(
+        <div className="game-mode">
+          <p>Select a play mode</p>
+          <button className="btn-mode" onClick={() => setMode("easy")}>
+            Easy
+          </button>
+          <button className="btn-mode" onClick={() => setMode("medium")}>
+            Medium
+          </button>
+          <button className="btn-mode" onClick={() => setMode("hard")}>
+            Hard
+          </button>
+        </div>
+        {
           <Link className="btn btn--start" to="game">
             Start quiz
           </Link>
-        )}
+        }
       </div>
-    </>
   );
 }
