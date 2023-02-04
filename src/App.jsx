ba3dc39c9ff7c7d84b826f1eb5e0bce08680c8ba
abeonweb <bmospecific@yahoo.com>
2023-02-04 07:04:21 +0100

    useReducer added to affect boolean on mode select

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import StartPage from "./StartPage";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
@@ -8,15 +8,30 @@ import Register from "./Register";
 import Game from "./Game";
 import { GameContext } from "./GameContext";
 
+function reducer(state, action){
+  if(action.type === "mode"){
+    return {
+      ...state,
+      mode: action.value,
+      start: true
+    }
+  }
+}
+
+const initialState = {
+  mode: "easy",
+  start: false
+}
+
 export default function App() {
-  const [mode, setMode] = useState("easy");
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <GameContext.Provider value={mode}>
+    <GameContext.Provider value={state}>
       <main className="main">
         <Routes>
           <Route path="/quizzical-game" element={<Layout />}>
-            <Route index element={<StartPage setMode={setMode} />} />
+            <Route index element={<StartPage state={state} dispatch={dispatch} />} />
             <Route path="game" element={<Game />} />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
