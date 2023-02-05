38bf2fcdbf1e01ba7d672b21bd18902af3d338d5
abeonweb <bmospecific@yahoo.com>
2023-02-05 23:25:59 +0100

    add title and subtitle

--- src/components/AccountBox.jsx
+++ src/components/AccountBox.jsx
@@ -5,7 +5,9 @@ import styled from "styled-components"
 const BoxContainer = styled.div`
     width: 300px;
     height: 70vh;
-    border: 1px solid gray;
+    border: 2px solid #e2e2e2;
+    border-radius: 20px;
+    box-shadow: 5px 6px 10px rgba(15, 15, 15, 0.3);
 `
 
 const FormTitle = styled.h1`
@@ -13,17 +15,29 @@ const FormTitle = styled.h1`
   color: #000;
   font-weight: 700;
   text-align: center;
-  padding: 1em 0;;
+  padding: 1em 0;
+  margin: 0;
+`
+
+const FormSubTitle = styled.h2`
+  font-size: 18px;
+  color: #000;
+  font-weight: 400;
+  text-align: center;
+  padding: 0;
+  margin: 0;
 `
 
 const Backdrop = styled.div`
   
 `
-const AccountBox = () => {
+const AccountBox = (props) => {
+  const {title, subtitle} = props
   return (
     <div>
         <BoxContainer>
-          <FormTitle>Login</FormTitle>
+          <FormTitle>{title}</FormTitle>
+          <FormSubTitle>{subtitle}</FormSubTitle>
           </BoxContainer>
     </div>
   )
