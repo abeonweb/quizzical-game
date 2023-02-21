99a44e4043cac0372ffb0d927e4607ddf110e124
abeonweb <bmospecific@yahoo.com>
2023-02-21 22:24:36 +0100

    import Buttun component format and clean up

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,35 +1,35 @@
-import { Link } from "react-router-dom";
+import Button from "./components/Button";
 
-export default function StartPage(props) {
-  const {state, dispatch}= props
-  
+export default function StartPage({start, dispatch}) {
   return (
-    
-      <div className="start-page">
-        <h1 className="start-page--title">quizzical</h1>
-        <p className="start-page--tagline">
-          A fun trivia game you can enjoy with friends!
-        </p>
-        <div className="game-mode">
-          <p>Select a play mode</p>
-          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "easy"})}>
-            Easy
-          </button>
-          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "medium"})}>
-            Medium
-          </button>
-          <button className="btn-mode" onClick={() => dispatch({type:"mode", value: "hard"})}>
-            Hard
-          </button>
-        </div>
-        
-          <Link 
-          className={`btn ${!state.start? "btn--disabled": null}`} 
-          to="game"
-          >
-            Start quiz
-          </Link>
-        
+    <div className="start-page">
+      <h1 className="start-page--title">quizzical</h1>
+      <p className="start-page--tagline">
+        A fun trivia game you can enjoy with friends!
+      </p>
+      <div className="game-mode">
+        <p>Select a play mode</p>
+        <button
+          className="btn-mode"
+          onClick={() => dispatch({ type: "mode", value: "easy" })}
+        >
+          Easy
+        </button>
+        <button
+          className="btn-mode"
+          onClick={() => dispatch({ type: "mode", value: "medium" })}
+        >
+          Medium
+        </button>
+        <button
+          className="btn-mode"
+          onClick={() => dispatch({ type: "mode", value: "hard" })}
+        >
+          Hard
+        </button>
       </div>
+
+      <Button start={start} text={"Start quiz"} />
+    </div>
   );
 }
