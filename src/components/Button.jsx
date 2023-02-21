af7404d0128fe4138807efcf9cca984ffbc61339
abeonweb <bmospecific@yahoo.com>
2023-02-21 22:25:20 +0100

    created button component as a link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const StyledButton = styled.div`
+const StyledButton = styled(Link)`
   font-size: 1rem;
   background-color: #4d5b9e;
   color: #f5f7fb;
-  text-decoration: none;
   padding: 1em 2.4em;
   border-radius: 10px;
   border: none;
   cursor: ${({ start }) => (start ? "allow" : "not-allowed")};
   opacity: ${({ start }) => (start ? 1 : 0.7)};
+  text-decoration: none;
 `;
 
 const Button = ({ start, text }) => {
   return (
-    <Link to="game">
-      <StyledButton start={start}>{text}</StyledButton>
-    </Link>
+    <StyledButton to="game" start={start}>
+      {text}
+    </StyledButton>
   );
 };
 
