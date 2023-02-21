94bed32498512f3d2d2f460cda204f7a42cfe045
abeonweb <bmospecific@yahoo.com>
2023-02-21 21:32:12 +0100

    fixed imported images path

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import blue from "./assets/blue.svg";
-import yellow from "./assets/yellow.svg";
+import blue from "../assets/blue.svg";
+import yellow from "../assets/yellow.svg";
 
 const Layout = () => {
   return (
