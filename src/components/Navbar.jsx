a917904afe5b673deba070ed140846a77d7c96b3
abeonweb <bmospecific@yahoo.com>
2023-02-22 16:56:36 +0100

    changed elements to styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+const StyledList = styled.ul`
+  display: flex;
+  justify-content: flex-end;
+  list-style: none;
+`
+
+const ListItem = styled.li`
+  padding: 0 0.5em;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #293264;
+`
 
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <Link to="login">Login</Link>
-          </li>
-          <li>
-            <Link to="register">Register</Link>
-          </li>
-        </ul>
+        <StyledList>
+          <ListItem>
+            <StyledLink to="login">Login</StyledLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink to="register">Register</StyledLink>
+          </ListItem>
+        </StyledList>
       </nav>
     </header>
   );
