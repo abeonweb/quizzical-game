e42c089f093c80e289d5697306b9f863d3502cf0
abeonweb <bmospecific@yahoo.com>
2023-02-25 23:48:59 +0100

    form and input added to componet

--- src/components/AccountBox.jsx
+++ src/components/AccountBox.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import styled from "styled-components"
-
+import React from "react";
+import styled from "styled-components";
 
 const BoxContainer = styled.div`
-    width: 300px;
-    height: 70vh;
-    border: 2px solid #e2e2e2;
-    border-radius: 20px;
-    box-shadow: 5px 6px 10px rgba(15, 15, 15, 0.3);
-`
+  width: 300px;
+  height: 70vh;
+  border: 2px solid #e2e2e2;
+  border-radius: 20px;
+  box-shadow: 5px 6px 10px rgba(15, 15, 15, 0.3);
+`;
 
 const FormTitle = styled.h1`
   font-size: 25px;
@@ -17,7 +16,7 @@ const FormTitle = styled.h1`
   text-align: center;
   padding: 1em 0;
   margin: 0;
-`
+`;
 
 const FormSubTitle = styled.h2`
   font-size: 18px;
@@ -25,22 +24,36 @@ const FormSubTitle = styled.h2`
   font-weight: 400;
   text-align: center;
   padding: 0;
-  margin: 0;
+  margin: 0 0 2em;
+`;
+
+const Backdrop = styled.div``;
+
+const StyledForm = styled.form`
+  padding: 0.5em;
+`
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 0.5em;
+  margin: 0 0 1em ;
+  border-radius: 6px;
 `
 
-const Backdrop = styled.div`
-  
-`
 const AccountBox = (props) => {
-  const {title, subtitle} = props
+  const { title, subtitle } = props;
   return (
     <div>
-        <BoxContainer>
-          <FormTitle>{title}</FormTitle>
-          <FormSubTitle>{subtitle}</FormSubTitle>
-          </BoxContainer>
+      <BoxContainer>
+        <FormTitle>{title}</FormTitle>
+        <FormSubTitle>{subtitle}</FormSubTitle>
+        <StyledForm>
+          <StyledInput type={"email"} placeholder={"email"} />
+          <StyledInput type={"password"} placeholder={"password"} />
+          <StyledButton type={"button"}>Sign In</StyledButton>
+        </StyledForm>
+      </BoxContainer>
     </div>
-  )
-}
+  );
+};
 
-export default AccountBox
+export default AccountBox;
