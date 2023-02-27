629bb3db380ec404b7a93219ec3677946d459caf
abeonweb <bmospecific@yahoo.com>
2023-02-27 22:04:46 +0100

    login form styles

--- src/components/AccountBox.jsx
+++ src/components/AccountBox.jsx
@@ -7,50 +7,76 @@ const BoxContainer = styled.div`
   border: 2px solid #e2e2e2;
   border-radius: 20px;
   box-shadow: 5px 6px 10px rgba(15, 15, 15, 0.3);
-`;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  `;
 
 const FormTitle = styled.h1`
   font-size: 25px;
-  color: #000;
+  color: #fff;
   font-weight: 700;
   text-align: center;
   padding: 1em 0;
   margin: 0;
-`;
+  position: relative;
+  z-index: 10;
+  `;
 
 const FormSubTitle = styled.h2`
   font-size: 18px;
-  color: #000;
+  color: #fff;
   font-weight: 400;
   text-align: center;
   padding: 0;
   margin: 0 0 2em;
-`;
+  position: relative;
+  z-index: 10;
+  `;
 
-const Backdrop = styled.div``;
+const Backdrop = styled.div`
+  height: 530px;
+  width: 160%;
+  position: absolute;
+  top: -290px;
+  left: -70px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(60deg);
+  background: #fdfd0a;
+  background: linear-gradient(
+    58deg,
+    rgba(241, 196, 15, 1) 20%,
+    rgba(243, 172, 18, 1) 100%
+  );
+  z-index: 5;
+`;
 
 const StyledForm = styled.form`
   padding: 0.5em;
-`
+`;
 const StyledInput = styled.input`
   width: 100%;
   padding: 0.5em;
-  margin: 0 0 1em ;
+  margin: 0 0 1em;
   border-radius: 6px;
-`
+`;
 
 const StyledButton = styled.button`
   width: 100%;
-  padding: 0.5em;
-  margin: 0 0 1em ;
+  padding: 0.8em 0.5em;
+  margin: 0 0 1em;
   border-radius: 6px;
-  border:  2px solid #c6c602;
-  background-color: #fdfd0a;
+  border: none;
+  background-color: rgba(241, 196, 15, 1);
 
   &:hover {
     box-shadow: 4px 5px 10px 1px rgb(192, 192, 192);
   }
-`
+`;
 
 const AccountBox = (props) => {
   const { title, subtitle } = props;
@@ -59,6 +85,7 @@ const AccountBox = (props) => {
       <BoxContainer>
         <FormTitle>{title}</FormTitle>
         <FormSubTitle>{subtitle}</FormSubTitle>
+        <Backdrop />
         <StyledForm>
           <StyledInput type={"email"} placeholder={"email"} />
           <StyledInput type={"password"} placeholder={"password"} />
