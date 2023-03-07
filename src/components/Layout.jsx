05a40adf1903ad4d5eee3439720c62a274eb3ca8
abeonweb <bmospecific@yahoo.com>
2023-03-07 23:08:59 +0100

    fixed issue with fixed screen height

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 
 const BackgroundLayout = styled.main`
   width: 100%;
-  height: 90vh;
+  min-height: 90vh;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
