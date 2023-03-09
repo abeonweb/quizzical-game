c5408823e4b46201b9becd6149e1a2617aedc4fd
abeonweb <bmospecific@yahoo.com>
2023-03-09 22:37:51 +0100

    updated to styled components

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,28 +1,52 @@
 import StyledLink from "./components/StyledLink";
+import styled from "styled-components";
 
-export default function StartPage({mode, start, dispatch}) {
+const StartPageContainer = styled.div`
+  font-family: "Inter", sans-serif;
+  background-color: transparent;
+  color: #293264;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-family: "Karla", sans-serif;
+  color: #293264;
+  text-transform: capitalize;
+  margin-bottom: 0.5em;
+`
+
+const Tagline = styled.p`
+  line-height: 1.5;
+  margin-bottom: 2em;
+`
+
+export default function StartPage({ mode, start, dispatch }) {
   return (
-    <div className="start-page">
-      <h1 className="start-page--title">quizzical</h1>
-      <p className="start-page--tagline">
+    <StartPageContainer>
+      <Title>quizzical</Title>
+      <Tagline>
         A fun trivia game you can enjoy with friends!
-      </p>
+      </Tagline>
       <div className="game-mode">
         <p>Select a play mode</p>
         <button
-          className={`${mode == "easy"? "highlight btn-mode": "btn-mode"}`}
+          className={`${mode == "easy" ? "highlight btn-mode" : "btn-mode"}`}
           onClick={() => dispatch({ type: "mode", value: "easy" })}
         >
           Easy
         </button>
         <button
-          className={`${mode == "medium"? "highlight btn-mode": "btn-mode"}`}
+          className={`${mode == "medium" ? "highlight btn-mode" : "btn-mode"}`}
           onClick={() => dispatch({ type: "mode", value: "medium" })}
         >
           Medium
         </button>
         <button
-          className={`${mode == "hard"? "highlight btn-mode": "btn-mode"}`}
+          className={`${mode == "hard" ? "highlight btn-mode" : "btn-mode"}`}
           onClick={() => dispatch({ type: "mode", value: "hard" })}
         >
           Hard
@@ -30,6 +54,6 @@ export default function StartPage({mode, start, dispatch}) {
       </div>
 
       <StyledLink start={start} link={"game"} text={"Start quiz"} />
-    </div>
+    </StartPageContainer>
   );
 }
